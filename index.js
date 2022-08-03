7b92d41cdb63f54f3b6d36a4032115e00de92f59
Raja Sekhar Pigilam <razsekar@gmail.com>
2022-08-03 14:21:57 +0530

    Only Phone filter is added to process

--- index.js
+++ index.js
@@ -27,3 +27,21 @@ app.get('/phone/:phone', (req, res) => {
     output = {phone};
     res.send(output)
 })
+
+app.get('/only-phone/:phone', (req, res) => {
+  // this is wrapped in an `async` function
+  // you can use await throughout the function
+  let phone = req.params.phone;
+  output = {phone};
+  if(phone.length == 10)
+  output = {phone: phone};
+  else 
+  if(phone.length == 12 && phone.substring(0, 2) == '91') //919985044678
+  output = {phone: phone.substring(2,13)};
+  else 
+  if(phone.length == 13 && phone.substring(1,3) == '91') //+919985044678
+  output = {phone: phone.substring(3,13)};
+  else
+  output = {phone};
+  res.send(output)
+})
